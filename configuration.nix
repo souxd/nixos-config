cdde0d85f41109e7c543facc5956eece9c232921
John Fernandes <souxd@proton.me>
2022-09-10 18:44:15 -0300

    i got this right

--- configuration.nix
+++ configuration.nix
@@ -33,8 +33,8 @@
   time.timeZone = "Brazil/East";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "pt_BR.UTF-8";
-  i18n.extraLocaleSettings = [ "en_US.UTF-8" ];
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = { LC_TIME = "pt_BR.UTF-8"; } 
   console = {
     font = "Lat2-Terminus16";
     keyMap = "br-abnt2";
