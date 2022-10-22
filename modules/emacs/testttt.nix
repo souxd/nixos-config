f4178e65f5395cdb57056addf71e80cd14f3f59d
souxd <souxd@proton.me>
2022-10-22 20:07:53 -0300

    forgot to remove some stuff

--- modules/emacs/testttt.nix
+++
@@ -1,5 +0,0 @@
-  doom-emacs = pkgs.callPackage (builtins.fetchTarball {
-    url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
-  }) {
-    doomPrivateDir = ./doom.d;
-  };
