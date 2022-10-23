329f9e9db114a424ab0be6c6f327ded2622da370
souxd <souxd@proton.me>
2022-10-22 21:34:43 -0300

    minor formatting change

--- modules/default.nix
+++ modules/default.nix
@@ -17,12 +17,12 @@
   };  
  
   nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = [
-  pkgs.nixFlakes
-  pkgs.man-db
-  pkgs.git
-  pkgs.neovim
-  pkgs.wget
+  environment.systemPackages = with pkgs; [
+  nixFlakes
+  man-db
+  git
+  neovim
+  wget
   ];
 
   environment.shellAliases = { nix-query = "nix-store -q --references /run/current-system/sw | rg -v man | sed 's/^[^-]*-//g' | sed 's/-[0-9].*//g' | rg -v '^nix' | sort -u"; };
