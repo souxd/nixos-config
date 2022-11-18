fe77a232188e4ed18c874f16ff25a0e6bd940e3f
souxd <souxd@proton.me>
2022-11-18 16:30:30 -0300

    better cachix.nix
    	modified:   modules/cachix.nix

--- modules/cachix.nix
+++ modules/cachix.nix
@@ -1,13 +1,13 @@
 # WARN: this file will get overwritten by $ cachix use <name>
 { pkgs, lib, ... }:
 
+let
+  folder = ./cachix;
+  toImport = name: value: folder + ("/" + name);
+  filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
+  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
+in
 {
-  imports = [
-    ./cachix/nix-community.nix
-    ./cachix/hyprland.nix
-  ];
-
+  inherit imports;
   nix.settings.substituters = [ "https://cache.nixos.org/" ];
-
-  environment.systemPackages = with pkgs; [ cachix ];
 }
