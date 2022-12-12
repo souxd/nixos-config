4fcd0e31bbe129839af48fa279c108c12238905b
souxd <souxd@proton.me>
2022-12-12 13:49:09 -0300

    use pkgs.firefox-beta & reorganize config
    	modified:   home/modules/firefox.nix

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -1,4 +1,4 @@
-{ config, specialArgs, ... }:
+{ config, pkgs, specialArgs, ... }:
 
 let
   inherit (specialArgs) ff-addons;
@@ -9,10 +9,25 @@ let
       display: none;
     }
   '';
+
+  sharedSettings = {
+    "browser.shell.checkDefaultBrowser" = false;
+    "app.normandy.first_run" = false;
+    "app.shield.optoutstudies.enabled" = true;
+    "browser.aboutConfig.showWarning" = false;
+
+    # disable updates
+    "app.update.auto" = false;
+    "extensions.update.enabled" = false;
+
+    "extension.activeThemeID" = "firefox-compact-dark@mozilla.org";
+  };
+
 in
 {
   programs.firefox = {
     enable = true;
+
     extensions = with ff-addons; [
       tridactyl
       auto-tab-discard
@@ -26,17 +41,14 @@ in
       don-t-fuck-with-paste
       h264ify
     ];
+
+    package = pkgs.firefox-beta-bin;
+
     profiles.default = {
       id = 0;
       name = "default";
       isDefault = true;
-      settings = {
-        "app.update.auto" = false;
-        "browser.shell.checkDefaultBrowser" = false;
-        "browser.aboutConfig.showWarning" = false;
-        "extensions.update.enabled" = false;
-        "extension.activeThemeID" = "firefox-compact-dark@mozilla.org";
-      };
+      settings = sharedSettings;
       userChrome = disableWebRtcIndicator;
     };
   };
