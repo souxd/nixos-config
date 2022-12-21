0a3656c95c50f5b8bc8a189c2f00f17ec9b93fe7
souxd <souxd@proton.me>
2022-12-21 19:12:49 -0300

    organize home-manager
    	modified:   home/souxd/shell.nix

--- home/souxd/shell.nix
+++ home/souxd/shell.nix
@@ -1,6 +1,13 @@
 { config, ... }:
 
 {
+  home.sessionVariables = {
+    EDITOR = "emacsclient -c";
+    VISUAL = "emacsclient -c";
+    MOZ_USE_XINPUT2 = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    WINEDLLOVERRIDES = "winemenubuilder.exe=d";
+  };
 
   programs.starship.enable = true;
   programs.bash = {
@@ -34,19 +41,5 @@
         fi
       }
     '';
-  };
-
-  home.sessionVariables = rec {
-    XDG_CACHE_HOME = "\${HOME}/.cache";
-    XDG_CONFIG_HOME = "\${HOME}/.config";
-    XDG_DATA_HOME = "\${HOME}/.local/share";
-    XDG_STATE_HOME = "\${HOME}/.local/state";
-    EDITOR = "emacsclient -c";
-    VISUAL = "emacsclient -c";
-    MOZ_USE_XINPUT2 = "1";
-    MOZ_ENABLE_WAYLAND = "1";
-    WINEDLLOVERRIDES = "winemenubuilder.exe=d";
-    NIX_PATH =
-      "nixpkgs=${config.xdg.configHome}/nix/inputs/nixpkgs\${NIX_PATH:+:$NIX_PATH}";
   };
 }
