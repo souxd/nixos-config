b0c952a058769a0f922d152ceee00cf5ca103420
souxd <souxd@proton.me>
2022-12-22 11:53:34 -0300

    remove mako dracula theme, it's unreadable for me
    	modified:   modules/home-manager/mako.nix

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -5,20 +5,5 @@
     enable = true;
 
     font = "FiraCode Nerd Font";
-
-    backgroundColor = "#282a36";
-    textColor = "#44475a";
-    borderColor = "#282a36";
-
-    extraConfig = ''
-      [urgency=low]
-      border-color=#282a36
-
-      [urgency=normal]
-      border-color=#f1fa8c
-
-      [urgency=high]
-      border-color=#ff5555
-    '';
   };
 }
