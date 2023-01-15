a2f1eabb365aae264c52a87554716f8290442c02
souxd <souxd@proton.me>
2023-01-15 18:40:35 -0300

    new configs for i2p profile
    	modified:   modules/home-manager/web-browsers/firefox.nix

--- modules/home-manager/web-browsers/firefox.nix
+++ modules/home-manager/web-browsers/firefox.nix
@@ -21,6 +21,76 @@ let
     "extension.activeThemeID" = "firefox-compact-dark@mozilla.org";
   };
 
+  i2p = {
+    "media.peerConnection.ice.proxy_only" = true;
+  };
+
+  privacySettings = {
+    ## https://miloslav.website/blog/2020/10/26/firefox-privacy
+    "browser.safebrowsing.allowOverride" = false;
+    "browser.safebrowsing.blockedURIs.enabled" = false;
+    "browser.safebrowsing.debug" = false;
+    "browser.safebrowsing.downloads.enabled" = false;
+    "browser.safebrowsing.downloads.remote.block_dangerous" = false;
+    "browser.safebrowsing.downloads.remote.block_dangerous_host" = false;
+    "browser.safebrowsing.downloads.remote.block_potentially_unwanted" = false;
+    "browser.safebrowsing.downloads.remote.block_uncommon" = false;
+    "browser.safebrowsing.downloads.remote.enabled" = false;
+    "browser.safebrowsing.downloads.remote.timeout_ms" = 15000;
+    "browser.safebrowsing.downloads.remote.url" = "https://127.0.0.1";
+    "browser.safebrowsing.id" = "null";
+    "browser.safebrowsing.malware.enabled" = false;
+    "browser.safebrowsing.passwords.enabled" = false;
+    "browser.safebrowsing.phishing.enabled" = false;
+    "browser.safebrowsing.prefixset_max_array_size" = 524288;
+    "browser.safebrowsing.provider.google.advisoryName" = "Google Safe Browsing";
+    "browser.safebrowsing.provider.google.advisoryURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google.gethashURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google.lists" = "";
+    "browser.safebrowsing.provider.google.pver" = "2.2";
+    "browser.safebrowsing.provider.google.reportMalwareMistakeURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google.reportPhishMistakeURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google.reportURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google.updateURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google4.advisoryName" = "Google Safe Browsing";
+    "browser.safebrowsing.provider.google4.advisoryURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google4.dataSharing.enabled" = false;
+    "browser.safebrowsing.provider.google4.dataSharingURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google4.gethashURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google4.lastupdatetime" = 1603739550029;
+    "browser.safebrowsing.provider.google4.lists" = "";
+    "browser.safebrowsing.provider.google4.nextupdatetime" = 1603741356029;
+    "browser.safebrowsing.provider.google4.pver" = 4;
+    "browser.safebrowsing.provider.google4.reportMalwareMistakeURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google4.reportPhishMistakeURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google4.reportURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.google4.updateURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.mozilla.gethashURL" = "https://127.0.0.1";
+    "browser.safebrowsing.provider.mozilla.lastupdatetime" = "1603739273301";
+    "browser.safebrowsing.provider.mozilla.lists" = "";
+    "browser.safebrowsing.provider.mozilla.lists.base" = "moz-std";
+    "browser.safebrowsing.provider.mozilla.lists.content" = "moz-full";
+    "browser.safebrowsing.provider.mozilla.nextupdatetime" = 1603742873301;
+    "browser.safebrowsing.provider.mozilla.pver" = "2.2";
+    "browser.safebrowsing.provider.mozilla.updateURL" = "https://127.0.0.1";
+    "browser.safebrowsing.reportPhishURL" = "https://127.0.0.1";
+    "services.sync.prefs.sync.browser.safebrowsing.downloads.enabled" = false;
+    "services.sync.prefs.sync.browser.safebrowsing.downloads.remote. block_potentially_unwanted" = false;
+    "services.sync.prefs.sync.browser.safebrowsing.malware.enabled" = false;
+    "services.sync.prefs.sync.browser.safebrowsing.phishing.enabled" = false;
+    "browser.safebrowsing" = false;
+
+    # Additional
+    "privacy.resistFingerprinting" = true;
+    "toolkit.telemetry.enabled" = false;
+    "datareporting.healthreport.service.enabled" = false;
+    "datareporting.healthreport.uploadEnabled" = false;
+    "media.eme.enabled" = false;
+    "media.gmp-eme-adobe.enabled" = false;
+    "browser.pocket.enabled" = false;
+    "geo.enabled" = false;
+  };
+
 in
 {
   programs.firefox = {
@@ -46,6 +116,14 @@ in
       name = "default";
       isDefault = true;
       settings = sharedSettings;
+      userChrome = disableWebRtcIndicator;
+    };
+
+    profiles.itoopeer = {
+      id = 1;
+      name = "itoopeer";
+      isDefault = false;
+      settings = sharedSettings // privacySettings // i2p;
       userChrome = disableWebRtcIndicator;
     };
   };
