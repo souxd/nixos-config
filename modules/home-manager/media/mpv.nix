b8525ea2249fd28fb09a4d2e862d4648b97cca9b
souxd <souxd@proton.me>
2023-02-19 08:15:02 -0300

    configuring mpv
    	modified:   modules/home-manager/media/mpv.nix

--- modules/home-manager/media/mpv.nix
+++ modules/home-manager/media/mpv.nix
@@ -1,14 +1,33 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.mpv = {
     enable = true;
+    scripts = with pkgs; [
+      mpvScripts.mpris
+      mpvScripts.thumbnail
+      mpvScripts.mpv-playlistmanager
+      mpvScripts.autoload
+      mpvScripts.sponsorblock
+    ];
     config = {
       profile = "gpu-hq";
       hwdec = "auto-safe";
-      ao = "pipewire";
+
+      ## VIDEO ##
       vo = "gpu";
       gpu-context = "wayland";
+
+      ## AUDIO ##
+      ao = "pipewire";
+      audio-channels = "auto-safe";
+      af = "lavfi=[dynaudnorm]";
+
+      osc = "no";
+      ytdl-format = "bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best[height<=?720]";
+      script-opts = "ytdl_hook-ytdl_path=~/.nix-profile/bin/yt-dlp";
+
+      keep-open = "yes";
     };
   };
 }
