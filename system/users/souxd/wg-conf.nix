efd96659d2fa15c0ba59fd10f53fef7a207f4008
souxd <souxd@proton.me>
2023-02-19 08:14:37 -0300

    update for new cool network
    	modified:   system/users/souxd/wg-conf.nix

--- system/users/souxd/wg-conf.nix
+++ system/users/souxd/wg-conf.nix
@@ -10,7 +10,7 @@
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     cool = {
       # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.0.0.8/24" ];
+      ips = [ "172.20.172.3/24" ];
       listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
 
       # Path to the private key file.
@@ -25,15 +25,15 @@
 
         {
           # Public key of the server (not a file path).
-          publicKey = "VrLLQA5nG4Vh5gUga0i65Dvnq0tiz+OG7Ckck/3RS08=";
+          publicKey = "+7EMzXjUVtcRlUJb5bZuDDfl4r9D9nm7pZCdwjc8CQg=";
 
           # Forward all the traffic via VPN.
-          allowedIPs = [ "10.0.0.0/24" ];
+          allowedIPs = [ "172.20.172.0/24" ];
           # Or forward only particular subnets
           #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
 
           # Set this to the server IP and port.
-          endpoint = "45.77.223.220:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+          endpoint = "144.76.68.254:1935"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
 
           # Send keepalives every 25 seconds. Important to keep NAT tables alive.
           persistentKeepalive = 25;
