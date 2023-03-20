1ecd2fb8bd23b6a152a756173454e4606aedf9f2
souxd <souxd@proton.me>
2023-03-20 18:19:55 -0300

    new mpv config
    	modified:   modules/home-manager/media/mpv.nix

--- modules/home-manager/media/mpv.nix
+++ modules/home-manager/media/mpv.nix
@@ -5,29 +5,32 @@
     enable = true;
     scripts = with pkgs; [
       mpvScripts.mpris
-      mpvScripts.thumbnail
-      mpvScripts.mpv-playlistmanager
       mpvScripts.autoload
-      mpvScripts.sponsorblock
     ];
     config = {
-      profile = "gpu-hq";
-      hwdec = "auto-safe";
-
       ## VIDEO ##
-      vo = "gpu";
+      vo = "gpu-next";
       gpu-context = "wayland";
+      hwdec = "auto-safe";
+      # profile = "gpu-hq";
 
       ## AUDIO ##
       ao = "pipewire";
       audio-channels = "auto-safe";
       af = "lavfi=[dynaudnorm]";
 
-      osc = "no";
+      ## MISC ##
+      script-opts = "ytdl_hook-ytdl_path=${pkgs.yt-dlp}/bin/yt-dlp";
       ytdl-format = "bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best[height<=?720]";
-      script-opts = "ytdl_hook-ytdl_path=~/.nix-profile/bin/yt-dlp";
 
       keep-open = "yes";
+
+      # required so that the 2 UIs don't fight each other
+      osc = "no";
+      # uosc provides its own seeking/volume indicators, so you also don't need this
+      osd-bar = "no";
+      # uosc will draw its own window controls if you disable window border
+      border = "no";
     };
   };
 }
