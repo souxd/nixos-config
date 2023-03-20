f8b3bab9c54f62845f86e6058e50189866fae828
souxd <souxd@proton.me>
2023-03-20 18:10:55 -0300

    new transmission config
    	modified:   modules/nixos/networking/transmission.nix

--- modules/nixos/networking/transmission.nix
+++ modules/nixos/networking/transmission.nix
@@ -1,7 +1,16 @@
+# lightweight torrent daemon
 { config, pkgs, ... }:
 
 {
   services.transmission = {
     enable = true;
+    settings = {
+      peer-limit-global = 16;
+      peer-limit-per-torrent = 2;
+      speed-limit-down = 1500;
+      speed-limit-down-enabled = true;
+      speed-limit-up = 900;
+      speed-limit-up-enabled = true;
+    };
   };
 }
